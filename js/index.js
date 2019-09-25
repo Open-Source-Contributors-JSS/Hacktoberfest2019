241294758656c6b894c4b203239add9a6527c3fd
pragati <itispragativerma@gmail.com>
2019-09-26 00:15:04 +0530

    added features

--- js/index.js
+++ js/index.js
@@ -36,6 +36,13 @@ $(document).ready(function() {
   });
 });
 
+var card = $(".card");
+
+$(document).on("mousemove",function(e) {  
+  var ax = -($(window).innerWidth()/2- e.pageX)/20;
+  var ay = ($(window).innerHeight()/2- e.pageY)/10;
+  card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
+});
 
 //scroll reveal
 window.sr = ScrollReveal();
