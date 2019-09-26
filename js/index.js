d55c8c8ac0322565a569f913491cbf3397b01555
pragati <itispragativerma@gmail.com>
2019-09-26 13:47:19 +0530

    added countdown

--- js/index.js
+++ js/index.js
@@ -71,3 +71,27 @@ $('.counting').each(function() {
   
 
 });
+
+
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('Oct 31, 2019 00:00:00').getTime(),
+    x = setInterval(function() {
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day))+' : ',
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour))+' : ',
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute))+' : ',
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+      if (distance < 0) {
+      clearInterval(x);
+      
+      }
+
+    }, second)
