90b463bb2715d03c29f09ff51935b9a29d386081
abc <rajat2502@users.noreply.github.com>
2019-09-29 13:24:26 +0530

    intermediate folder updated

---
+++ Intermediate/algorithms/Hello_world.cpp
@@ -0,0 +1,13 @@
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello World"; 
+      
+    return 0; 
+} 
