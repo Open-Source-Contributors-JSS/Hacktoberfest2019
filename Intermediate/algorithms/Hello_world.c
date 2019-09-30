70e32d5ff3d54b94287077b868792f8daa190152
rsaksham <sakshamraghuvanshi1@gmail.com>
2019-10-01 00:13:24 +0530

    Hello_world.c file added

---
+++ Intermediate/algorithms/Hello_world.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+// main function
+int main()
+    {
+        // printf() command is used to output data on screen
+        printf("Hello World! Happy Hacktoberfest");
+        return 0;
+    }
