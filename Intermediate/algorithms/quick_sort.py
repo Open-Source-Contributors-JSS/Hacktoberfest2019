4a35c17488adcb3acec48d81b7e0a2f72a6aa2e9
Mikail Seremet <mikail.seremet@gmail.com>
2019-10-01 02:16:53 -0700

    Removed print statements

--- Intermediate/algorithms/quick_sort.py
+++ Intermediate/algorithms/quick_sort.py
@@ -19,8 +19,5 @@ def partition(arr, start, end):
   
 if __name__ == '__main__':
   arr = [11, 105, 16, 187, 64, 70, 199, 68, 102, 139, 125, 190, 88, 67, 53, 61, 127, 17, 142, 96, 166, 20, 47, 131, 36]
-  print(arr)
-  print()
   q_s(arr, 0, len(arr)-1)
-  print()
-  print(arr)
+
