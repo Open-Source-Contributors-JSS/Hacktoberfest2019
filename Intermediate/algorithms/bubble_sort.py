4ed43f78c7168ed1a490be53c0cee6a5b5b2c167
Matías Ortega <mortegalopez635@gmail.com>
2019-10-03 21:48:11 -0300

    Name bug solved
    Renamed bubble sort second method by Justin Giffard to evade replacement

--- Intermediate/algorithms/bubble_sort.py
+++ Intermediate/algorithms/bubble_sort.py
@@ -1,34 +1,24 @@
-#!/usr/bin/env python3
+def bubble_sort(array):
+	temp = 0
+	#  Counts number of Sorts
+	sort = 0
+	# Counts number of Runs
+	runs = 0
+	swap = True
+	for i in range(len(array)):
+		if swap is True:
+			runs +=1
+		swap = False
+		#  Prints each step of array
+		print(array)
+		for j in range(len(array) -1):
+			if array[j+1] < array[j]:
+				temp = array[j]
+				array[j] = array[j+1]
+				array[j+1] = temp
+				sort +=1
+				swap = True
+	return array
 
-"""
-Bubble Sort
-@author Justin Giffard
-
-This is a simple to understand sorting algorithm but not efficient for large lists,
-it has a time complexity of O(n^2) at worst and O(n) at best (averages O(n^2)).
-The way it works is it looks at the first element and compares it to each of the other
-elements and swops if the value is lower. Once it reaches the end of the list the smallest value
-is in the first position and then it moves to the second element and repeats. There is a small optimisation
-in this implementation that counts the number of changes in the iteration of the nested loop and if there
-are no swops then stops the outerloop as the list is sorted
-"""
-
-def bubbleSort(array):
-    n = len(array)
-    for i in range(n):
-        swops = 0
-        for j in range(i+1, n):
-            if array[j] < array[i]:
-               array[j], array[i] = array[i], array[j]
-               swops += 1
-        if swops == 0:
-            break
-
-print("Enter numbers seperated by a space then press enter:")
-arr = list(map(int,input().split()))
-n = len(arr)
-bubbleSort(arr)
-print("After sort: ")
-for i in range(0, n) :
-    print(arr[i], end=' ')
-print()
+test_array = [1,7,6,5,4,2,8,3,9]
+print(bubble_sort(test_array))
