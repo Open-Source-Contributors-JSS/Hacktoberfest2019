3e9e1d1cb18e55c462b077c35a4f75005e604b48
Micah Nagel <mjnagel@liberty.edu>
2019-10-04 23:12:31 -0400

    Added a integer reversal algorithm for C++

---
+++ Intermediate/algorithms/ReverseInteger.cpp
@@ -0,0 +1,11 @@
+class ReverseInteger {
+public:
+    int reverse(int x) {
+        long long res = 0;
+        while(x) {
+            res = res*10 + x%10;
+            x /= 10;
+        }
+        return (res<INT_MIN || res>INT_MAX) ? 0 : res;
+    }
+};
