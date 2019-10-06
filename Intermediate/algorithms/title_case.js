79d18c32b5fc1e8d65ec89da549aca19034c85c9
Kevin Harvell <kevharvell@gmail.com>
2019-10-05 21:16:53 -0500

    added algorithm for title casing a sentence

---
+++ Intermediate/algorithms/title_case.js
@@ -0,0 +1,9 @@
+function titleCase(str) {
+  str = str.toLowerCase().split(" ");
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(" ");
+}
+
+titleCase("this sentence will be title cased");
