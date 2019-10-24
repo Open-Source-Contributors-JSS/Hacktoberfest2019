0efb473d3c2c1460999f72afbfdbbd8bd798e058
Eduardo Nunes <edugenix@gmail.com>
2019-10-23 21:49:31 -0300

    added factorial algoritm

---
+++ Intermediate/algorithms/factorial.py
@@ -0,0 +1,14 @@
+'''
+Recursive Factorial
+
+This is a factorial algorithm using an easy recursive solution.
+'''
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(8))
+print(8 * factorial(7))
