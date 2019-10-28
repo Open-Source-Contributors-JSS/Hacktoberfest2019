d9d5154299b4a441d853d26034df4d02fc7d5e55
Amariah Del Mar <amariahdelmar@gmail.com>
2019-10-28 13:08:34 -0700

    pushing to move over to linux

---
+++ Intermediate/games/dnd_dice_roll.py
@@ -0,0 +1,14 @@
+''' This project is for hacktoberfest 2019. It is a dice roller for DND and other tabletop RPGs. The user will type
+        the dice they want to roll in the form of '2d6', '1d20', '3d10', etc. Will work on giving the option on giving multiple dice at once
+    Programmer: Amariah Del Mar / itstheholyginger
+ '''
+
+
+
+
+
+if __name__ == '__main__':
+    welcome()
+    get_user_input()
+    
+
