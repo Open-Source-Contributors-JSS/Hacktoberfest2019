8fc212ffb3b073cd66c2e2442301f0871f3c14ad
Mailson Reis <amaral17@gmail.com>
2019-10-30 22:15:58 -0300

    create function slug
    Function for creating slugs from a string.

---
+++ Intermediate/algorithms/slug.php
@@ -0,0 +1,15 @@
+<?php
+function slug($text)
+{
+  
+  $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+  $text = preg_replace('~[^-\w]+~', '', $text);
+  $text = trim($text, '-');
+  $text = preg_replace('~-+~', '-', $text);
+  $text = strtolower($text);
+  if (empty($text)) {
+    return 'n-a';
+  }
+  return $text;
+}
