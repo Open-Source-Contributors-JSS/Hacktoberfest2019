b7fe15952e46d0ed591043c13a21e784c32edc24
Sangram Gorai <sangramgorai@gmail.com>
2020-10-02 13:08:24 +0530

    Create app.component.ts
    added app.component

---
+++ app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [];
+ 
+
+  
+}
