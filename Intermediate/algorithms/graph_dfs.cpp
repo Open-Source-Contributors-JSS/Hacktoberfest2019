057cbf8d979a1b50123bef76748487c4d64a573c
paulocarneiro99 <paulo_carneiro@usp.br>
2020-10-20 20:10:03 -0300

    finding a graph topology using dfs algorithm

---
+++ Intermediate/algorithms/graph_dfs.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define visited 1
+
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+vi visit;
+vi predecessor;
+vector<vii> AdjList(100);
+vector<int> topologia;
+
+void dfs(int v){
+    visit[v] = visited;
+    for (int i = 0; i < (int)AdjList[v].size(); i++){
+        if (visit[AdjList[v][i].first] != visited){
+            predecessor[AdjList[v][i].first] = v;
+            dfs(AdjList[v][i].first);
+        }
+    }
+    topologia.pb(v);
+}
+
+int main(){
+    int v1, v2;
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++){
+        visit.pb(-1);
+        predecessor.pb(-2);
+    }
+    while (scanf("%d %d", &v1, &v2) != EOF){
+        if (v1 != 0 && v2 != 0){
+            AdjList[v1].pb(make_pair(v2, 0));
+        }
+    }
+    //generating the topoly of this graph using DFS
+    for (int i = 1; i < n; i++){
+        if (visit[i] != visited)
+            dfs(i);
+    }
+    for (int i = topologia.size() - 1; i >= 0; i--){
+        printf("%d ", topologia[i]);
+    }
+    printf("\n");
+    return 0;
+}
