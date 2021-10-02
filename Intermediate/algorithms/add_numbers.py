74514755b7fcf3abaae43fc6342b2b37cd5b7e4d
Md. Almas Ali <almaspr3@gmail.com>
2021-10-02 11:50:38 +0600

    Added Table genarator programe tables.py and added a f-string example in add_number.py programe.

--- Intermediate/algorithms/add_numbers.py
+++ Intermediate/algorithms/add_numbers.py
@@ -2,6 +2,7 @@
 """
 Python3 program to add two numbers
 @author swastikaushik
+@contributor almas-ali
 
 """
   
@@ -11,5 +12,8 @@ num2 = 12
 # Adding two nos 
 sum = num1 + num2 
   
-# printing values 
+# printing values with .format()
 print("Sum of {0} and {1} is {2}" .format(num1, num2, sum)) 
+
+# printing values with f-string
+print(f'Sum of {num1} and {num2} is {sum}')
---
+++ Intermediate/algorithms/tables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Python3 program to make tables
+@author almas-ali
+"""
+
+num = int(input('Enter a number : '))
+
+for i in range(1, 11):
+    print(f'{num} x {i} = {i*num}')
