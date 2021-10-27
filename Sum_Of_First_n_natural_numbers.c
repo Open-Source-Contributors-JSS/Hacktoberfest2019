808f673f3ddee2ecc8339214da16410e3d07d1a2
Rameshwar Singh <72304873+rameshwar-rajput@users.noreply.github.com>
2021-10-27 22:43:14 +0530

    Sum of n prime numbers

--- Sum_Of_First_n_natural_numbers.c
+++ Sum_Of_First_n_natural_numbers.c
@@ -1,16 +1,43 @@
-//This program will tell how to evaluate sum of n natural numbers in 0(1) time.
+/**
+ * C program to find sum of prime numbers between 1 to n
+ */
 
 #include <stdio.h>
 
-int main() {
-int num, sum = 0;
-printf ("Enter the last natural number you want the sum of: ");
-//Sum = 1+2+3.....n
-scanf("%d", &num);
-for (int i = 0; i<=num; i++)
+int main()
 {
-    sum +=i;
-}
-printf ("Sum of first %d natural numbers is : %d\n", num, sum);
-return 0;
+    int i, j, end, isPrime, sum=0;
+
+    /* Input upper limit from user */
+    printf("Find sum of all prime between 1 to : ");
+    scanf("%d", &end);
+
+    /* Find all prime numbers between 1 to end */
+    for(i=2; i<=end; i++)
+    {
+
+        /* Check if the current number i is Prime or not */
+        isPrime = 1;
+        for(j=2; j<=i/2 ;j++)
+        {
+            if(i%j==0)
+            {
+                /* 'i' is not prime */
+                isPrime = 0;
+                break;
+            }
+        }
+
+        /*
+         * If 'i' is Prime then add to sum
+         */
+        if(isPrime==1)
+        {
+            sum += i;
+        }
+    }
+
+    printf("Sum of all prime numbers between 1 to %d = %d", end, sum);
+
+    return 0;
 }
